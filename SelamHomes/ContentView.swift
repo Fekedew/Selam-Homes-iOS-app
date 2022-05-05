e2a865b21c00cf4494a5c21a03a690ab1955b842
Fekedew Hailemariam <softwarefeke@gmail.com>
2022-05-05 23:37:32 +0300

    first commit

--- SelamHomes/ContentView.swift
+++ SelamHomes/ContentView.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 
+extension View {
+    func toAnyView() -> AnyView {
+        AnyView(self)
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var showLoading: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+
+            TabView {
+                WebView(url: URL(string: "https://www.selamhomes.com")!, showLoading: $showLoading)
+                    .overlay(showLoading ? ProgressView("Loading...").toAnyView(): EmptyView().toAnyView())
+                
+                 .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+               }
+                
+                WebView(url: URL(string: "https://www.selamhomes.com/?post_type=hp_listing")!, showLoading: $showLoading)
+                    .overlay(showLoading ? ProgressView("Loading...").toAnyView(): EmptyView().toAnyView())
+                
+                 .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+              }
+                
+                WebView(url: URL(string: "https://www.selamhomes.com/add-listings/")!, showLoading: $showLoading)
+                    .overlay(showLoading ? ProgressView("Loading...").toAnyView(): EmptyView().toAnyView())
+                
+                 .tabItem {
+                    Image(systemName: "plus")
+                    Text("Add")
+              }
+                
+                WebView(url: URL(string: "https://www.selamhomes.com/account/")!, showLoading: $showLoading)
+                    .overlay(showLoading ? ProgressView("Loading...").toAnyView(): EmptyView().toAnyView())
+                
+                 .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Account")
+              }
+                
+            }
+        }
     }
 }
 
@@ -19,3 +62,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
