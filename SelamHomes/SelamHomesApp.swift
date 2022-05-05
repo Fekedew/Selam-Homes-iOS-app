384345588addf95a7bad9981e8f8d11ff8b02b30
Fekedew Hailemariam <softwarefeke@gmail.com>
2022-05-05 13:34:02 +0300

    Initial Commit

---
+++ SelamHomes/SelamHomesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelamHomesApp.swift
+//  SelamHomes
+//
+//  Created by Fekedew on 05/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SelamHomesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
